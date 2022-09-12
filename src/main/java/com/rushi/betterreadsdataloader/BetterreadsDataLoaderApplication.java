ea00d845d9138ec85bb7577638debac7cc5c61b1
Rushi <rushikeshgandhmal@gmail.com>
2022-09-12 15:45:56 +0530

    made changes

--- src/main/java/com/rushi/betterreadsdataloader/BetterreadsDataLoaderApplication.java
+++ src/main/java/com/rushi/betterreadsdataloader/BetterreadsDataLoaderApplication.java
@@ -150,7 +150,7 @@ public class BetterreadsDataLoaderApplication {
 	@PostConstruct
 	public void start() {
 
-		// initAuthors();
+		initAuthors();
 		initWorks();
 	}
 
--- src/main/java/connection/DataStaxAstraProperties.java
+++ src/main/java/connection/DataStaxAstraProperties.java
@@ -3,18 +3,17 @@ package connection;
 import java.io.File;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
-    
+
     private File secureConnectBundle;
 
-    public File getSecureConnectBundle() {        
+    public File getSecureConnectBundle() {
         return secureConnectBundle;
     }
 
     public void setSecureConnectBundle(File secureConnectBundle) {
         this.secureConnectBundle = secureConnectBundle;
-    }    
+    }
 }
